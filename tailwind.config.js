c3eb3fd1d14db9cbd365843d5823a9dda2c91aaf
kkafi09 <afi.firda@gmail.com>
2022-09-05 22:45:29 +0700

    first commit - made navbar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        cream: "#FCDCDC",
+        black: "#312A37"
+      },
+    },
+  },
+  plugins: [],
+};
